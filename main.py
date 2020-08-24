4a24c28bf98775d3c7e23ad6c458b516ff77291d
wambuguedison <wambuguedison@gmail.com>
2020-08-24 19:23:17 +0300

    Remove file name check

--- main.py
+++ main.py
@@ -40,6 +40,3 @@ def check_currency(currency):
     # Tell the user ISO code is not supported
     if not supported:
       click.echo('The currency - {} is not supported'.format(currency))
-
-if __name__ == "__main__":
-  check_currency()
