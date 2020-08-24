c365af800893adf0cc33c274524dba865ee1ecb2
wambuguedison <wambuguedison@gmail.com>
2020-08-24 16:25:07 +0300

    Validate ISO code format

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import click
 
 @click.command()
 @click.argument('currency', required=True)
-def cli_app(currency):
-  """This is a testing help"""
-  click.echo("testing " + currency)
+def check_currency(currency):
+  """Usage
+
+  check_currency 'currency'
+
+  - where currency is the ISO 4217 code
+
+  This command checks whether the currency is supported or not
+
+  """
+  if len(currency) != 3:
+    raise click.UsageError(
+        'The ISO code is a three-letter alphabetic code '
+        'and an equivalent three-digit numeric code'
+    )
 
 if __name__ == "__main__":
-  cli_app()
+  check_currency()
