fa12d6f7d29b64623ff33c7ea277c5200363c4ea
wambuguedison <wambuguedison@gmail.com>
2020-08-24 18:48:49 +0300

    test for wrong ISO format

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,9 @@
+from click.testing import CliRunner
+from main import check_currency
+
 def test_check_currency():
-    pass
+  runner = CliRunner()
+  result = runner.invoke(check-currency, ['XAFsad'])
+  assert result.exit_code == 1
+  assert result.output == 'The ISO code is a three-letter alphabetic code'
+        'and an equivalent three-digit numeric code'
